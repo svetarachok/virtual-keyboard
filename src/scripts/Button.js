4768de656c6e17379530eb97ee0a173400cbf0ad
Sveta Rachok <sveta.r1212@gmail.com>
2022-05-05 00:11:24 +0300

    feat: add css styles and render keybord with buttons to html

---
+++ src/scripts/Button.js
@@ -0,0 +1,58 @@
+export class Button {
+    constructor (key) {
+        this.key = key;
+        this.value = '';
+    }
+
+    generateButton () {
+        let button = document.createElement('button');
+        button.className = 'key';
+        button.innerHTML = `${this.key}`;
+        
+        if (this.key === 'Backspace' || this.key === "CapsLock" || this.key === "Shift" || this.key === "Enter") {
+            button.classList.add('key-wide')
+            button.classList.add('key-control')
+        }
+
+        if (this.key === 'Alt' || this.key === "Tab" || this.key === "Control") {
+            button.classList.add('key-control')
+        }
+
+        if (this.key === "Delete") {
+            button.classList.add('semi-wide')
+            button.classList.add('key-control')
+        }
+
+        if (this.key === "Meta") {
+            button.classList.add('key-control')
+            button.innerHTML = `<span class="material-icons">grid_view</span>`
+        }
+
+        if (this.key === ' ') {
+            button.classList.add('spacebar')
+            button.innerHTML = `<span class="material-icons">space_bar</span>`
+        }
+
+        if (this.key === 'ArrowLeft') {
+            button.classList.add('key-control')
+            button.innerHTML = `<span class="material-icons">arrow_left</span>`
+        }
+
+        if (this.key === 'ArrowRight') {
+            button.classList.add('key-control')
+            button.innerHTML = `<span class="material-icons">arrow_right</span>`
+        }
+
+        if (this.key === 'ArrowUp') {
+            button.classList.add('key-control')
+            button.innerHTML = `<span class="material-icons">arrow_drop_up</span>`
+        }
+
+        if (this.key === 'ArrowDown') {
+            button.classList.add('key-control')
+            button.innerHTML = `<span class="material-icons">arrow_drop_down</span>`
+        }
+        return button
+    }
+}
+
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,41 @@
 import '../style.css'
+import { Button} from './Button'
 
-console.log("Hello World!");
+const body = document.querySelector('body');
+const header = renderElement('header','header', 'Rss Virtual Keyboard');
+const main = renderElement('main', 'main');
+const keyboardWrapper = renderElement('div', 'keyboard_wrapper')
+const keyboard = renderElement('div', 'keyboard')
+
+
+function renderElement (element, name, content) {
+    let node = document.createElement(`${element}`);
+    node.className = name;
+    node.textContent = content;
+    return node
+}
+
+
+body.prepend(header);
+header.after(main)
+main.append(keyboardWrapper)
+keyboardWrapper.append(keyboard)
+
+const arrLowerCase = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete','CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control' 
+];
+
+const renderButtons =  (arr, container) => {
+    arr.forEach(button => {
+        let element = new Button(button)
+        container.append(element.generateButton())
+        return container
+    })
+}
+
+renderButtons(arrLowerCase, keyboard)
+
+
+// document.onkeydown = function (event) {
+//     arrLowerCase.push(event.key)
+//     console.log(arrLowerCase)
+// }
